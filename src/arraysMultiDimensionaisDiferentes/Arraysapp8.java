a37086df31ac0c5ed98fbcb7a710ecfcb17f23ea
rafaw <rafa.willian91@gmail.com>
2023-03-16 05:25:22 -0300

    Arrays multimensionais com tamanhos diferentes

--- src/arraysMultiDimensionaisDiferentes/Arraysapp8.java
+++ src/arraysMultiDimensionaisDiferentes/Arraysapp8.java
@@ -12,7 +12,7 @@ public class Arraysapp8 {
         array[2] = new int[3];
         array[3] = new int[5];
 
-
+//foreach interacao:
 //        for (int[] d1: array) {
 //
 //            for (int d2 : d1) {
@@ -22,18 +22,21 @@ public class Arraysapp8 {
 //
 //        }
 
+        //teste passagem de array MultiDimensional
         test(array);
         
     }
 
     private static void test(int[][]ar){
-        for (int[] d1: ar) {
+        for (int i = 0; i < ar.length; i++){
 
-            for (int d2 : d1) {
-                System.out.print(d2);
+            //cada interacao considera a quantidade de colunas para interar.Ex : ar.length vale 4 que é o tamanho total.
+            //Já ar[i].length vale 2 que é o total de colunas da linha 1 no caso 0 do array dimenssao 1.
+            for (int j = 0; j < ar[i].length; j++){
+
+                System.out.print(ar[i][j]);
             }
             System.out.println();
-
         }
     }
 }
