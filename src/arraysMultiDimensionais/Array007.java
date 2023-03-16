904861d0f41d3f0e7a48091ae4a65af2c0098452
rafaw <rafa.willian91@gmail.com>
2023-03-16 04:06:36 -0300

    Arrays MultiDimensionais inicializacao e interação

--- src/arraysMultiDimensionais/Array007.java
+++ src/arraysMultiDimensionais/Array007.java
@@ -9,21 +9,35 @@ public class Array007 {
 //        arrayM[2][1] = 6;
 
         //Inicialização de arrays Multidimensionais
-        int[][] arrayM = {{1,2},{3,5},{4,6}};
+        int[][] arrayM = {{1, 2}, {3, 5}, {4, 6}};
         // cada Chave é uma linha. Seria como está abaixo.
         /*{
             {1,2},
             {3,5},
             {4,6}
         };*/
-
-        for (int i = 0; i < arrayM.length; i++){
+        t(arrayM);
+        for (int i = 0; i < arrayM.length; i++) {
             //dentro do array tem outro array para segunda dimensao, por isso que é arrayM[i].length.
-            for (int j = 0; j < arrayM[i].length; j++){
+            for (int j = 0; j < arrayM[i].length; j++) {
                 System.out.print(arrayM[i][j]);
             }
             System.out.println();
         }
 
     }
+
+
+    private static void t(int[][] varargs) {//a copia da referencia para o heap é passada
+        System.out.println("_____________________________");
+        for (int i = 0; i < varargs.length; i++) {
+            //dentro do array tem outro array para segunda dimensao, por isso que é arrayM[i].length.
+            for (int j = 0; j < varargs[i].length; j++) {
+                System.out.print(varargs[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("--------------------------------");
+    }
+
 }
