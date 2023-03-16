3117d4f6d53dc101d7145ef169846b82ffe4ecee
rafaw <rafa.willian91@gmail.com>
2023-03-16 03:06:33 -0300

    Arrays MultiDimensionais

---
+++ src/arraysMultiDimensionais/Array007.java
@@ -0,0 +1,29 @@
+package arraysMultiDimensionais;
+
+public class Array007 {
+
+    public static void main(String[] args) {
+
+//        int[][] arrayM = new int [3][2];
+//        arrayM[0][0] = 4;
+//        arrayM[2][1] = 6;
+
+        //Inicialização de arrays Multidimensionais
+        int[][] arrayM = {{1,2},{3,5},{4,6}};
+        // cada Chave é uma linha. Seria como está abaixo.
+        /*{
+            {1,2},
+            {3,5},
+            {4,6}
+        };*/
+
+        for (int i = 0; i < arrayM.length; i++){
+            //dentro do array tem outro array para segunda dimensao, por isso que é arrayM[i].length.
+            for (int j = 0; j < arrayM[i].length; j++){
+                System.out.print(arrayM[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+}
