4b735d91bdeeab72c51d8042c8d89e2793f96728
rafaw <rafa.willian91@gmail.com>
2023-03-16 22:23:08 -0300

    Passando parametros para o main via linha de comando ou pela propria ide.

---
+++ src/ArraysCommandLine/CommandLine.java
@@ -0,0 +1,19 @@
+package ArraysCommandLine;
+
+import java.util.Arrays;
+
+public class CommandLine {
+    //os parametros são passados via linha de comando chamando java e a classe.java ou pela propria idea
+    //que passa no metodo main.
+    public static void main(String[] args) {
+
+
+
+        System.out.println(args[0] + " Willian");
+
+
+    }
+
+
+
+}
